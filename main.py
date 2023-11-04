3bf738601ff56995a8edc3f6a8a5fe52e94d8b9d
Marcos Dayan Mann <75103719+marcosdayanm@users.noreply.github.com>
2023-11-04 00:21:15 -0600

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -327,4 +327,5 @@ def schedule():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    port = int(os.getenv("PORT", default=5001))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
